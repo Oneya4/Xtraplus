842cbaf902ea8aeabd27f1d3c411368f77b0d3ed
Oneya4 <46073127+Oneya4@users.noreply.github.com>
2020-10-30 20:55:07 +0300

    first commit

---
+++ scripts.js
@@ -0,0 +1,6 @@
+function toggle(){
+	const toggler = document.querySelector('.toggle');
+	const navigation = document.querySelector('.navigation');
+	toggler.classList.toggle('active');
+	navigation.classList.toggle('active');
+}
