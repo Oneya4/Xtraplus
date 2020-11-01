da295810064fe20638dba065229103388917a01d
Oneya4 <46073127+Oneya4@users.noreply.github.com>
2020-11-01 22:29:51 +0300

    second commit

--- scripts.js
+++ scripts.js
@@ -4,3 +4,13 @@ function toggle(){
 	toggler.classList.toggle('active');
 	navigation.classList.toggle('active');
 }
+
+// image slider
+var counter = 1;
+	setInterval(function(){
+	    document.getElementById('radio' + counter).checked = true;
+	      counter++;
+	      if(counter > 4){
+	        counter = 1;
+	     }
+}, 4000);
