659d46ae19856a1e3d68a0bc6ab73824ed52fff4
Oneya4 <46073127+Oneya4@users.noreply.github.com>
2020-11-15 16:59:37 +0300

    first complete draft

--- scripts.js
+++ scripts.js
@@ -5,6 +5,27 @@ function toggle(){
 	navigation.classList.toggle('active');
 }
 
+//accordion
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+
+
 // image slider
 var counter = 1;
 	setInterval(function(){
